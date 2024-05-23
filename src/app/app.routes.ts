35b4e804f716ede76c95cf506b4945b1896ef02c
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-05-23 19:05:46 +0530

    Re-enable default route redirection to '/home' in AppRoutingModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
